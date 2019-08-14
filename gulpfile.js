91c362780acd1486efef72bff33b3495f1dd41ad
Pier Herbozo <pier.herbozo@sap.com>
2019-08-13 20:41:06 -0500

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -23,10 +23,7 @@ var ghPages = require('gulp-gh-pages');
 gulp.task('deploy', function () {
   return gulp.src('./dist/Angular-Lazy-Loading/*')
     .pipe(ghPages({ 
-      origin: "https://github.com/REIP/demoDeplyment.git",
-      remote: "https://REIP:rivera1234@github.com/REIP/project.git",
-      force:true,
-      push:true
+      origin: "https://github.com/REIP/demoDeplyment.git"
     }))
 });
 
